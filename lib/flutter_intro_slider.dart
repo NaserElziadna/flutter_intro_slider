a24a862ccaf0dfdc525396ec617638bf0dde84a6
elziadna10@gmail.com <elziadna10@gmail.com>
2021-06-26 14:33:06 +0300

    migrate to null safety first demo not reusable

--- lib/flutter_intro_slider.dart
+++ lib/flutter_intro_slider.dart
@@ -1,14 +1,1 @@
-
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-
-class FlutterIntroSlider {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_intro_slider');
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-}
+export 'package:flutter_intro_slider/src/intro_slider.dart';
